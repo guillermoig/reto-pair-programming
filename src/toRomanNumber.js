b9cb34981e1fd1b1a1d93eacedf6d585bc44af51
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2022-09-19 19:54:21 +0200

    green

--- src/toRomanNumber.js
+++ src/toRomanNumber.js
@@ -2,28 +2,33 @@ const dic = {
   // 1: "I",
   5: "V",
   10: "X",
-  50: "L"
+  50: "L",
+  100: "C",
+  500: "D",
+  1000: "M"
 }
 
 const toRomanNumber = (number) => {
-
   let result = ""
   Object.keys(dic).reverse().forEach(key => {
-    if (number === key - 1) {
-      result += "I" + dic[key]
-      number -= key - 1
-      return
-    }
+    const rounds = Math.ceil(number + 1 / key);
 
-    if(number / key < 1) {
-      return
+    for (let index = 0; index < rounds; index++) {   
+      if (number === key - 1) {
+        result += "I" + dic[key]
+        number -= key - 1
+        return
+      }
+  
+      if(number / key < 1) {
+        return
+      }
+  
+      if ((number / key) >= 1) {
+        number -= key
+        result += dic[key]
+      }
     }
-
-    if ((number / key) >= 1) {
-      number -= key
-      result += dic[key]
-    }
-
   })
 
 	return result + "I".repeat(number);
--- src/toRomanNumber.test.js
+++ src/toRomanNumber.test.js
@@ -15,8 +15,16 @@ const testCases = [
   { input: 14, expected: "XIV" },
   { input: 15, expected: "XV" },
   { input: 19, expected: "XIX" },
-  // { input: 20, expected: "XX" },
-  // { input: 24, expected: "XXIV" },
+  { input: 20, expected: "XX" },
+  { input: 24, expected: "XXIV" },
+  { input: 25, expected: "XXV" },
+  { input: 26, expected: "XXVI" },
+  { input: 29, expected: "XXIX" },
+  { input: 30, expected: "XXX" },
+  { input: 38, expected: "XXXVIII" },
+  { input: 39, expected: "XXXIX" },
+  { input: 65, expected: "LXV" },
+  { input: 2022, expected: "MMXXII" },
 ];
 
 testCases.forEach(({ input, expected }) => {
