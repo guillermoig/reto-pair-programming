72f176406da1391b71024f59c748edc8682c7158
Guillermo Ibanez <guillermoig@gmail.com>
2022-09-19 18:59:48 +0200

    Guillermo paso 2

--- src/toRomanNumber.js
+++ src/toRomanNumber.js
@@ -7,14 +7,23 @@ const dic = {
 
 const toRomanNumber = (number) => {
   if(number < 4) return "I".repeat(number)
-  if(number === 4) return "IV"
-  if(number === 5) return "V"
-  if(number === 6) return "VI"
-  if(number === 7) return "VII"
-  if(number === 8) return "VIII"
-  if(number === 9) return "IX"
+  if(number === 10) return "X"
+  if ((number % 5) === 4) {
+    return 'I' + dic[number + 1]
+  }
 
-  return "X"
+  if ((number / 5) >= 1) {
+    return 'V' + 'I'.repeat(number % 5)
+  }
+
+  // if(number === 4) return "IV"
+  // if(number === 5) return "V"
+  // if(number === 6) return "VI"
+  // if(number === 7) return "VII"
+  // if(number === 8) return "VIII"
+  // if(number === 9) return "IX"
+
+  // return "X"
 }
 
 export default toRomanNumber
