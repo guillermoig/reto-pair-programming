f5caed5fe192d832744f891ba8b7ef1c186d3c5d
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2022-09-19 19:23:57 +0200

    wip

--- src/toRomanNumber.js
+++ src/toRomanNumber.js
@@ -6,24 +6,20 @@ const dic = {
 }
 
 const toRomanNumber = (number) => {
-  if(number < 4) return "I".repeat(number)
-  if(number === 10) return "X"
-  if ((number % 5) === 4) {
-    return 'I' + dic[number + 1]
-  }
+  if(number=== 9) return "IX"
+  if(number === 4) return "IV"
 
-  if ((number / 5) >= 1) {
-    return 'V' + 'I'.repeat(number % 5)
-  }
+  let result = ""
+  Object.keys(dic).reverse().forEach(key => {
+    if(number / key < 1) {
+      return
+    }
 
-  // if(number === 4) return "IV"
-  // if(number === 5) return "V"
-  // if(number === 6) return "VI"
-  // if(number === 7) return "VII"
-  // if(number === 8) return "VIII"
-  // if(number === 9) return "IX"
-
-  // return "X"
+    number -= key
+    result += dic[key]
+  })
+  
+	return result + "I".repeat(number);
 }
 
 export default toRomanNumber
