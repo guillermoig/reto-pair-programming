3697e0b4e8ae8f3b539a3ab2b2eeb51b70921668
Guillermo Ibanez <guillermoig@gmail.com>
2022-10-10 19:17:04 +0200

    Second step

--- src/gameOfLife/gameOfLife.js
+++ src/gameOfLife/gameOfLife.js
@@ -1,6 +1,24 @@
-export default function gameOfLife({ width = 8, height = 4 } = {}) {
-    const rows = Array.from({ length: width }).fill(".")
-    const board = Array.from({length: height}).fill(rows);
-    
-    return board
+export default function gameOfLife( board ) {
+    const result = []
+    for (let y = 0; y < board.length; y++){
+      for (let x = 0; x < board.length; x++){
+        const topLeft = board[y-1][x-1]?? 0
+        const topCenter = board[y-1][x]?? 0
+        const topRigth = board[y-1][x+1]?? 0
+        const centerLeft = board[y][x-1]?? 0
+        const centerRigth = board[y][x+1]?? 0
+        const bottomLeft = board[y+1][x-1]?? 0
+        const bottomCenter = board[y+1][x]?? 0
+        const bottomRigth = board[y+1][x+1]?? 0
+
+        const sum = topLeft + topCenter + topRigth + centerLeft + centerRigth + bottomLeft + bottomCenter + bottomRigth
+        if (sum === 3) {
+          result[y][x] = 1
+        }
+        else {
+          result[y][x] = board[y][x]
+        }
+      }
+    }
+    return result
 }
--- src/gameOfLife/gameOfLife.test.js
+++ src/gameOfLife/gameOfLife.test.js
@@ -1,23 +1,45 @@
 import gameOfLife from './gameOfLife';
 
 describe('gameOfLife', () => {
-    test("Should be a board 4x8 by default", () => {
-        const game = gameOfLife();
-        expect(game).toEqual([
-            [".", ".", ".", ".", ".", ".", ".", "."],
-            [".", ".", ".", ".", ".", ".", ".", "."],
-            [".", ".", ".", ".", ".", ".", ".", "."],
-            [".", ".", ".", ".", ".", ".", ".", "."],
+    test("Should be the first board", () => {
+        const initialBoard = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+        const nextGen = gameOfLife(initialBoard);
+        expect(nextGen).toEqual([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
         ]);
     })
-    test("Should be a customizable board", () => {
-        const game = gameOfLife({
-            width: 2,
-            height: 2,
-        });
-        expect(game).toEqual([
-            [".", "."],
-            [".", "."],
+    test("Should be the first board", () => {
+        const initialBoard = [
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0],
+        ]
+        const nextGen = gameOfLife(initialBoard);
+        expect(nextGen).toEqual([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
         ]);
     })
+    // test("Should be the first board", () => {
+    //     const initialBoard = [
+    //         [0, 0, 0, 0, 0, 0, 0, 0],
+    //         [0, 0, 0, 0, 1, 0, 0, 0],
+    //         [0, 0, 0, 1, 1, 0, 0, 0],
+    //         [0, 0, 0, 0, 0, 0, 0, 0],
+    //     ]
+    //     const nextGen = gameOfLife(initialBoard);
+    //     expect(nextGen).toEqual([
+    //         [0, 0, 0, 0, 0, 0, 0, 0],
+    //         [0, 0, 0, 1, 1, 0, 0, 0],
+    //         [0, 0, 0, 1, 1, 0, 0, 0],
+    //         [0, 0, 0, 0, 0, 0, 0, 0],
+    //     ]);
+    // })
 });
