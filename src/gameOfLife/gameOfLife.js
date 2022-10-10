c94575a3886addcd37a5bd5fc55a5bf03da1674e
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2022-10-10 19:35:53 +0200

    wip

--- src/gameOfLife/gameOfLife.js
+++ src/gameOfLife/gameOfLife.js
@@ -1,24 +1,42 @@
+const clone = obj => JSON.parse(JSON.stringify(obj))
+const getNeighbors = ({ x, y }, board) => {
+    const topLeft = board[y-1]?.[x-1]
+    const topCenter = board[y-1]?.[x]
+    const topRight = board[y-1]?.[x+1]
+    const centerLeft = board[y]?.[x-1]
+    const centerRight = board[y]?.[x+1]
+    const bottomLeft = board[y+1]?.[x-1]
+    const bottomCenter = board[y+1]?.[x]
+    const bottomRight = board[y+1]?.[x+1]
+
+    return [
+        topLeft,
+        topCenter,
+        topRight,
+        centerLeft,
+        centerRight,
+        bottomLeft,
+        bottomCenter,
+        bottomRight
+    ].filter(cell => cell !== undefined)
+} 
 export default function gameOfLife( board ) {
-    const result = []
+    const result = clone(board);
+
     for (let y = 0; y < board.length; y++){
       for (let x = 0; x < board.length; x++){
-        const topLeft = board[y-1][x-1]?? 0
-        const topCenter = board[y-1][x]?? 0
-        const topRigth = board[y-1][x+1]?? 0
-        const centerLeft = board[y][x-1]?? 0
-        const centerRigth = board[y][x+1]?? 0
-        const bottomLeft = board[y+1][x-1]?? 0
-        const bottomCenter = board[y+1][x]?? 0
-        const bottomRigth = board[y+1][x+1]?? 0
+        const neightbors = getNeighbors({x, y}, board);
+        const sum = neightbors.reduce((a, b) => a + b, 0);
 
-        const sum = topLeft + topCenter + topRigth + centerLeft + centerRigth + bottomLeft + bottomCenter + bottomRigth
-        if (sum === 3) {
-          result[y][x] = 1
+        if (result[y][x] === 1 & sum < 3 ) {
+            result[y][x] = 0
         }
+
         else {
           result[y][x] = board[y][x]
         }
       }
     }
+
     return result
 }
