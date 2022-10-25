63dbe886597087ce95486921b6347c205e1d0551
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2022-10-25 09:16:03 +0200

    private cell state

--- src/gameOfLife/gameOfLife/Cell.js
+++ src/gameOfLife/gameOfLife/Cell.js
@@ -4,32 +4,35 @@ const STATE_SYMBOL = {
 }
 
 class Cell {
+    #state = STATE_SYMBOL.dead
+    #neightborsCount = 0
+    
     constructor( state ) {
-      this.state = state;
+      this.#state = state;
     }
     
     isAlive() {
-      return this.state === STATE_SYMBOL.alive
+      return this.#state === STATE_SYMBOL.alive
     }
     
     setNeighbors(neightbors) {
-      this.neightborsCount = neightbors.length;
+      this.#neightborsCount = neightbors.length;
     }
   
     next() {
-      this.state = this.#shouldbeAlive() ? STATE_SYMBOL.alive : STATE_SYMBOL.dead    
+      this.#state = this.#shouldbeAlive() ? STATE_SYMBOL.alive : STATE_SYMBOL.dead    
     }
   
     render() {
-      return this.state
+      return this.#state
     }
 
     #shouldbeAlive() {
       if(this.isAlive()){
-        return this.neightborsCount === 2 || this.neightborsCount === 3
+        return this.#neightborsCount === 2 || this.#neightborsCount === 3
       }
       
-      return this.neightborsCount === 3
+      return this.#neightborsCount === 3
     }
 }
 
